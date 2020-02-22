f1556ee6c25bc8561bb9bf19ec2c81f3454bf257
Marc Marza <marcmarzaosa@gmail.com>
2020-02-22 18:01:55 +0100

    He añadido datos a la base de datos.

--- UF2Practica2/Controller/LoginViewController.swift
+++ UF2Practica2/Controller/LoginViewController.swift
@@ -48,17 +48,22 @@ class LoginViewController: UIViewController {
     func loadEvents(){
         DBManager.sharedInstance.addData(object: Event(country: "Espana", name: "Vuelta España", popularity: 10, numberStage: 2, stageKm: 23))
         DBManager.sharedInstance.addData(object: Event(country: "Francia", name: "Tour de France", popularity: 30, numberStage: 2, stageKm: 23))
+        DBManager.sharedInstance.addData(object: Event(country: "Italia", name: "Giro d'Italia", popularity: 15, numberStage: 5, stageKm: 40))
+        DBManager.sharedInstance.addData(object: Event(country: "Bélgica", name: "Tour de Flandes", popularity: 5, numberStage: 7, stageKm: 50))
     }
         
     func loadCyclists(){
         DBManager.sharedInstance.addData(object: Cyclist(team: "Movistar", country: "España", specialty: "Pedalear", lastName: "Contador", firstName: "Alberto", birthDate: "22/12/1990", popularity: 90, leader: true, size: 190, weight: 70, mountain: 20, plain: 50, downhilling: 10, sprint: 10, resistance: 70, recuperation: 25, timeTrial: 34))
             
-        DBManager.sharedInstance.addData(object: Cyclist(team: "Amarillo", country: "Francia", specialty: "Molestar", lastName: "Croissant", firstName: "Françoise", birthDate: "22/12/1995", popularity: 80, leader: false, size: 190, weight: 70, mountain: 20, plain: 50, downhilling: 10, sprint: 10, resistance: 70, recuperation: 25, timeTrial: 34))
+        DBManager.sharedInstance.addData(object: Cyclist(team: "BH", country: "Francia", specialty: "DownHiling", lastName: "Martinez", firstName: "Fernando", birthDate: "22/12/1993", popularity: 60, leader: false, size: 170, weight: 67, mountain: 30, plain: 50, downhilling: 10, sprint: 3, resistance: 70, recuperation: 25, timeTrial: 34))
+        DBManager.sharedInstance.addData(object: Cyclist(team: "Sky", country: "Italia", specialty: "Sprint", lastName: "Quintana", firstName: "Nairo", birthDate: "22/12/1995", popularity: 85, leader: false, size: 190, weight: 75, mountain: 30, plain: 55, downhilling: 5, sprint: 40, resistance: 80, recuperation: 20, timeTrial: 33))
+        DBManager.sharedInstance.addData(object: Cyclist(team: "Orbea", country: "España", specialty: "Mountain", lastName: "Rodríguez", firstName: "Joaquim", birthDate: "22/12/1985", popularity: 75, leader: false, size: 180, weight: 76, mountain: 40, plain: 50, downhilling: 10, sprint: 30, resistance: 90, recuperation: 25, timeTrial: 37))
     }
         
     func loadTeams(){
-        DBManager.sharedInstance.addData(object: Team(country: "España", name: "Movistar", manager: "Marc Marzà"))
-        DBManager.sharedInstance.addData(object: Team(country: "Francia", name: "Eiffel", manager: "Romain Grosjean"))
+        DBManager.sharedInstance.addData(object: Team(country: "España", name: "Movistar", manager: "Alberto Rodriguez"))
+        DBManager.sharedInstance.addData(object: Team(country: "Francia", name: "Sky", manager: "Miguel Casanova"))
+        DBManager.sharedInstance.addData(object: Team(country: "Francia", name: "Orbea", manager: "Juan Fernandez"))
     }
 
 }
